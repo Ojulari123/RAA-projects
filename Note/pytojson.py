792a2a8dec55d0c3fcb829feee14e4c20b5038db
OJ <adeoluwaojulari@gmail.com>
2024-06-06 23:37:06 -0400

    new pytojson

--- Note/pytojson.py
+++ Note/pytojson.py
@@ -1,8 +1,24 @@
 import json
+import os 
 
 with open("notes.txt","r") as file:
-    print_file = file.readlines()
+   read_note = file.readlines()
 
-output = open("notes.json","w")
-json.dump(print_file, output)
-output.close()
+note_data = {}
+for note in read_note:
+        parts = note.split(" : ", 1)
+        timestamp = f"{parts[0]}"
+        note_content = parts[1].strip()
+        note_data[timestamp] = note_content
+    
+name = input("What is your name? ")
+year = int(input("What is your year of birth? "))
+
+dic = {
+    "name" : name,
+    "year_of_birth" : year,
+    "notes": note_data
+}
+
+with open("notes.json","w") as file:
+    file.write(json.dumps(dic, indent=4))
